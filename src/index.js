af76828bf8de73662140e164d33b971662491907
evag2582 <elio.g28@gmail.com>
2023-11-19 18:36:52 -0400

    update task 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import axios from "axios";
 
-// axios.defaults.headers.common["x-api-key"] = "live_gmXn9iScvnIM6VnMhaM5HChycxoWgFXopptFBEOmol0oCCJxDXRGxkY9WzowbtPx";
+// axios.defaults.headers.common["x-api-key"] = "live_gmXn9iScvnIM6VnMhaM5HChycxoWgFXopptFBEOmol0oCCJxDXRGxkY9WzowbtPx";/
 import { fetchBreeds } from "./cat-api.js";
         
         fetchBreeds();
